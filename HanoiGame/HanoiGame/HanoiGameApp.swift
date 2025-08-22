0ade54a4f27600d7dfb05fd7a567448b994d966a
Alberto Orrantia <ricco.albert@gmail.com>
2025-08-22 01:14:45 -0600

    chore: project setup

---
+++ HanoiGame/HanoiGame/HanoiGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanoiGameApp.swift
+//  HanoiGame
+//
+//  Created by Alberto Orrantia on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HanoiGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

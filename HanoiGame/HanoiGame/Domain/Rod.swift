6eb79b3d589218e557f2d51b845a2144f796b930
Alberto Orrantia <ricco.albert@gmail.com>
2025-08-23 00:07:57 -0600

    chore: Add MVVM and Clean Architecture Structure, Add app entry

---
+++ HanoiGame/HanoiGame/Domain/Rod.swift
@@ -0,0 +1,14 @@
+//
+//  Rod.swift
+//  HanoiGame
+//
+//  Created by Alberto Orrantia on 23/08/25.
+//
+
+import Foundation
+
+enum Rod {
+    case a
+    case b
+    case c
+}
--- HanoiGame/HanoiGame/Support/HanoiGameApp.swift
+++ HanoiGame/HanoiGame/Support/HanoiGameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HanoiGameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
